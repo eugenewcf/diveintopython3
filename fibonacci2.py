398f38b762d72311f810d23b6832b2564e53927f
Eugene Wong <eugene.wong@razerzone.com>
2015-10-02 10:39:19 +0800

    Fix bug on file.

--- fibonacci2.py
+++ fibonacci2.py
@@ -2,15 +2,15 @@ class Fib:
     '''iterator that yields numbers in the Fibonacci sequence'''
 
     def __init__(self, max):
-        
+
         self.max = max
 
     def __iter__(self):
-        
+
         self.a = 0
-        
+
         self.b = 1
-        
+
         return self
 
     def __next__(self):
@@ -18,7 +18,7 @@ class Fib:
         fib = self.a
 
         if fib > self.max:
-            rise StopIteration
+            raise StopIteration
 
         self.a, self.b = self.b, self.a + self.b
 
